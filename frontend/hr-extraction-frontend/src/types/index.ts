e62712219f1d0ceded1bbfb9b7a6982b19f8f6d4
HARI <harishsubramanian2@gmail.com>
2025-04-27 20:30:41 +0530

    changes in the css and alingments and then removed commetings

--- frontend/hr-extraction-frontend/src/types/index.ts
+++ frontend/hr-extraction-frontend/src/types/index.ts
@@ -32,55 +32,6 @@ export enum Role {
     created_at: string;
     updated_at: string | null;
   }
-  
-  // export interface DashboardStats {
-  //   total_files: number;
-  //   success_files: number;
-  //   error_files: number;
-  //   //recent_files: ProcessedFile[];
-  //   pf_files?: number;       // Optional breakdown
-  //   esi_files?: number;      // Optional breakdown
-  //   pf_success?: number;     // Optional breakdown
-  //   pf_error?: number;       // Optional breakdown
-  //   esi_success?: number;    // Optional breakdown
-  //   esi_error?: number;
-  //   remittance_stats: {
-  //     total_submitted: number; // Total remittance submissions
-  //     pending: number; // Pending remittance submissions
-  //     timely_submissions?: number;
-  //   };
-  //   monthly_stats: {
-  //     labels: string[]; // Array of month names (e.g., ["January", "February"])
-  //     datasets: {
-  //       pf_success: number[]; // Monthly PF success counts
-  //       pf_error: number[]; // Monthly PF error counts
-  //       esi_success: number[]; // Monthly ESI success counts
-  //       esi_error: number[]; // Monthly ESI error counts
-  //       remittance_submitted: number[]; // Monthly remittance submissions
-  //     };
-  //   };
-  //   recent_files: {
-  //     id: number; // File ID
-  //     filename: string; // File name
-  //     status: string; // File status (e.g., "success", "error")
-  //     created_at: string; // File creation date
-  //     filepath: string; // File path (used to determine type: PF or ESI)
-  //   }[];
-  //   user_activity?: {
-  //     top_users: {
-  //       username: string; // User's name
-  //       pf_files: number; // Number of PF files processed by the user
-  //       esi_files: number; // Number of ESI files processed by the user
-  //     }[];
-  //     total_users?: number;
-  //   };
-  //   remittance_delays:{
-  //     days:number;
-  //     count:number;
-  //     type:'PF' | 'ESI';
-  //   }[];
-
-  // }
    export interface ChartData {
     labels: string[];
     data: number[];
